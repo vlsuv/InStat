b688f9bd95df14d5cd52f61168301e598e10d699
Vlad Suchkov <vsuchkov96@gmail.com>
2021-06-15 15:39:17 +0400

    initial commit

---
+++ InStat_vlsuv/MatchStateModule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InStat_vlsuv
+//
+//  Created by vlsuv on 15.06.2021.
+//  Copyright © 2021 vlsuv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
